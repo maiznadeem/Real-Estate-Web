4e588888e8a328134824f4dd270c40aaf4beede6
MaizNadeem <maiznadeem616@gmail.com>
2023-07-27 17:05:39 +0500

    Value Section Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
-
-import './App.css'
 import Companies from "./components/Companies/Companies"
 import Residencies from "./components/Residencies/Residencies"
+import Value from "./components/Value/Value"
+
+import './App.css'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
             </div>
             <Companies />
             <Residencies />
+            <Value />
         </div>
     )
 }
