4b3e9ccdd286e4632d9a22846f017576ad365f61
MaizNadeem <maiznadeem616@gmail.com>
2023-07-27 15:01:10 +0500

    Frontend in progress.

---
+++ client/src/components/Companies/Companies.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Companies.css'
+
+const Companies = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth flexCenter c-container">
+                <img src="./prologis.png" alt="Proglogis" />
+                <img src="./tower.png" alt="Tower" />
+                <img src="./equinix.png" alt="Equinix" />
+                <img src="./realty.png" alt="Realty" />
+            </div>
+        </section>
+    )
+}
+
+export default Companies
