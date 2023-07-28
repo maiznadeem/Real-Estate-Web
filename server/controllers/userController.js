6a8157d4552eb2239957b6e2161e9729c8cfc20d
MaizNadeem <maiznadeem616@gmail.com>
2023-07-29 00:44:09 +0500

    Controllers Complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,3 +39,88 @@ export const bookVisit = asyncHandler(async (req, res) => {
         throw new Error(err.message)
     }
 })
+
+export const getAllBookings = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    try {
+        const bookings = await prisma.user.findUnique({
+            where: {email: email},
+            select: {bookedVisits: true}
+        })
+        res.status(200).send(bookings)
+    } catch (err) {
+        throw new Error(err.message)
+    }
+})
+
+export const cancelBooking = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    const { id } = req.params
+    try {
+        const user = await prisma.user.findUnique({
+            where: {email: email},
+            select: {bookedVisits: true}
+        })
+        const index = user.bookedVisits.findIndex( visit => visit.id === id )
+        if (index === -1) {
+            res.status(404).json({message: "Booking not found!"})
+        } else {
+            user.bookedVisits.splice(index, 1)
+            await prisma.user.update({
+                where: {email: email},
+                data: {
+                    bookedVisits: user.bookedVisits
+                }
+            })
+            res.send("Booking canceled successfully.")
+        }
+    } catch (err) {
+        throw new Error(err.message)
+    }
+})
+
+export const manageFavourites = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    const { id } = req.params
+    try {
+        const user = await prisma.user.findUnique({
+            where: {email: email}
+        })
+        if (user.favResidenciesID.includes(id)) {
+            const updatedUser = await prisma.user.update({
+                where: {email: email},
+                data: {
+                    favResidenciesID: {
+                        set: user.favResidenciesID.filter( rid => rid !== id )
+                    }
+                }
+            })
+            res.send({message: "Removed from favourites.", user: updatedUser})
+        } else {
+            const updatedUser = await prisma.user.update({
+                where: {email: email},
+                data: {
+                    favResidenciesID: {
+                        push: id
+                    }
+                }
+            })
+            res.send({message: "Added to favourites.", user: updatedUser})
+        }
+    } catch (err) {
+        throw new Error(err.message)
+    }
+})
+
+export const getAllFavourites = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    try {
+        const favResidenciesID = await prisma.user.findUnique({
+            where: {email: email},
+            select: {favResidenciesID: true}
+        })
+        res.status(200).send(favResidenciesID)
+    } catch (err) {
+        throw new Error(err.message)
+    }
+})
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,14 @@
 import express from 'express'
 
-import { bookVisit, createUser } from '../controllers/userController.js'
+import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFavourites, manageFavourites } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.post("/register", createUser)
 router.post("/bookvisit/:id", bookVisit)
+router.post("/getallbookings", getAllBookings)
+router.post("/cancelbooking/:id", cancelBooking)
+router.post("/managefavourites/:id", manageFavourites)
+router.post("/getallfavourites", getAllFavourites)
 
 export { router as userRoute }
