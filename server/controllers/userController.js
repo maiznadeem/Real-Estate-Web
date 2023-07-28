8983e9b994decdc1c394251fd1d04ad8f5dce8b6
MaizNadeem <maiznadeem616@gmail.com>
2023-07-28 23:17:04 +0500

    Secret Key Hidden

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,6 @@ import asyncHandler from 'express-async-handler'
 import { prisma } from '../config/prismaConfig.js'
 
 export const createUser = asyncHandler(async (req, res) => {
-    console.log("Creating a User... ")
-
     let {email} = req.body
     const userExists = await prisma.user.findUnique({where: {email: email}})
     if (!userExists) {
@@ -14,6 +12,30 @@ export const createUser = asyncHandler(async (req, res) => {
             user: user,
         })
     }
-    else 
+    else
         res.status(201).send({message: "User already registered."})
+})
+
+export const bookVisit = asyncHandler(async (req, res) => {
+    const { email, date } = req.body
+    const { id } = req.params
+    try {
+        const alreadyBooked = await prisma.user.findUnique({
+            where: {email: email},
+            select: {bookedVisits: true}
+        })
+        if (alreadyBooked.bookedVisits.some( visit => visit.id === id )) {
+            res.status(400).json({message: "You've already booked this place."})
+        } else {
+            await prisma.user.update({
+                where: {email: email},
+                data: {
+                    bookedVisits: {push: {id, date}}
+                }
+            })
+            res.send({message: "Booked successfully."})
+        }
+    } catch (err) {
+        throw new Error(err.message)
+    }
 })
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { createUser } from '../controllers/userController.js'
+import { bookVisit, createUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.post("/register", createUser)
+router.post("/bookvisit/:id", bookVisit)
 
 export { router as userRoute }
