f0c0991b217b13d9a08c1c9edc57f342570cd223
MaizNadeem <maiznadeem616@gmail.com>
2023-07-31 23:39:14 +0500

    Auh0 keys changed

--- client/config.js
+++ client/config.js
@@ -2,7 +2,7 @@
 const config = {
   domain: 'dev-ccw5bra5gc5nxss7.us.auth0.com',
   clientID: 'MsaoJnPD45w9wnvB0ePTBYHbeLkKZdh8',
-  clientSecret: 'ZG6mnz9f2lknqau4eU7ND5sRQewjCLCuGAIAAfWPTrYkmFm6OtiHGOm6y9mojLs4',
+  clientSecret: 'MhaMp6nsfHjuG73MMoL6w7Hl6bPOLhZjiRc8dz46U3py48RmGG8hHyFQIHHwyinm',
 };
 
 export default config;
