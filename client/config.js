0d100afda11483debdebb2f463a939e8b8c38fe7
MaizNadeem <maiznadeem616@gmail.com>
2023-08-01 00:00:53 +0500

    User Profile started

--- client/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const config = {
-  domain: 'dev-ccw5bra5gc5nxss7.us.auth0.com',
-  clientID: 'MsaoJnPD45w9wnvB0ePTBYHbeLkKZdh8',
-  clientSecret: 'MhaMp6nsfHjuG73MMoL6w7Hl6bPOLhZjiRc8dz46U3py48RmGG8hHyFQIHHwyinm',
-};
-
-export default config;
