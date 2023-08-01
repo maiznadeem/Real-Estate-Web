e787ed7f0306390a716ecd445e0e425a58e95e5f
MaizNadeem <maiznadeem616@gmail.com>
2023-08-01 17:07:13 +0500

    User Context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
@@ -9,31 +9,39 @@ import Layout from './components/Layout/Layout'
 import Website from "./pages/Website"
 import Properties from './pages/Properties/Properties'
 import Property from './pages/Property/Property'
+import UserDetailsContext from './context/UserDetailsContext'
 
 import './App.css'
 
 function App() {
 
     const queryClient = new QueryClient()
+    const [ userDetails, setUserDetails ] = useState({
+        favourites: [],
+        bookings: [],
+        token: null
+    })
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route element={<Layout />}>
-                            <Route path="/" element={<Website />} />
-                            <Route path='/properties' >
-                                <Route index element={<Properties />} />
-                                <Route path=":propertyID" element={<Property />} />
+        <UserDetailsContext.Provider value={{userDetails, setUserDetails}}>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route element={<Layout />}>
+                                <Route path="/" element={<Website />} />
+                                <Route path='/properties' >
+                                    <Route index element={<Properties />} />
+                                    <Route path=":propertyID" element={<Property />} />
+                                </Route>
                             </Route>
-                        </Route>
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
-            <ToastContainer />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+                <ToastContainer />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </UserDetailsContext.Provider>
     )
 }
 
