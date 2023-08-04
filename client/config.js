0cbc0f26cecb9f7510ad94351eeab5963ad0b351
MaizNadeem <maiznadeem616@gmail.com>
2023-08-04 16:41:05 +0500

    Config on client side

---
+++ client/config.js
@@ -0,0 +1,10 @@
+// config.js is changed now
+
+const config = {
+    domain: 'dev-ccw5bra5gc5nxss7.us.auth0.com',
+    clientID: 'o7gQXTaqFsiQS1RMLLYpqSQF8EFtorQY',
+    cloudName: "dwujonydh",
+    uploadPreset: "lvzberqd",
+};
+
+export default config;
