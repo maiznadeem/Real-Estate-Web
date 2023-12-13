f4ea5bcbef78dc9396c027b837baf73fc7c8290f
MaizNadeem <maiznadeem616@gmail.com>
2023-12-14 01:45:32 +0500

    Audience for JWT Auth

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,8 @@
 import { auth } from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: 'http://localhost:8000',
+    // audience: 'http://localhost:8000',
+    audience: 'https://real-estate-82xr1ywy5-maiznadeem.vercel.app',
     issuerBaseURL: 'https://dev-ccw5bra5gc5nxss7.us.auth0.com',
     tokenSigningAlg: 'RS256'
 })
