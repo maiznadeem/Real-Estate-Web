56629b89b48a5aded92cc75ee6cd1dc62755bf0e
MaizNadeem <maiznadeem616@gmail.com>
2023-12-14 02:35:23 +0500

    Audience changed in Auth0

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -27,7 +27,8 @@ const Layout = () => {
             try {
                 const res = await getAccessTokenWithPopup({
                     authorizationParams: {
-                        audience: "http://localhost:8000",
+                        // audience: "http://localhost:8000",
+                        audience: "https://real-estate-web-lac.vercel.app",
                         scope: "openid profile email",
                     }
                 })
--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import { auth } from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
     // audience: 'http://localhost:8000',
-    audience: 'https://real-estate-web-lac.vercel.app/api',
+    audience: 'https://real-estate-web-lac.vercel.app',
     issuerBaseURL: 'https://dev-ccw5bra5gc5nxss7.us.auth0.com',
     tokenSigningAlg: 'RS256'
 })
